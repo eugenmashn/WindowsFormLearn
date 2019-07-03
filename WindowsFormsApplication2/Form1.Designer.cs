1325e0677b83b92bcc5c1fc8a0e50dd8b398ebb7
eugenmashn <36498074+eugenmashn@users.noreply.github.com>
2019-07-03 15:57:07 +0300

    Add files via upload

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Year = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dayDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.peopleBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.peopleBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.Year,
+            this.nameDataGridViewTextBoxColumn,
+            this.lastNameDataGridViewTextBoxColumn,
+            this.dayDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.peopleBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(552, 241);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Year
+            // 
+            this.Year.DataPropertyName = "Year";
+            this.Year.HeaderText = "Year";
+            this.Year.Name = "Year";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(135, 295);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Add day holidays";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(24, 295);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(281, 295);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Show date";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(468, 266);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Sort";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(468, 295);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Next Year";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // lastNameDataGridViewTextBoxColumn
+            // 
+            this.lastNameDataGridViewTextBoxColumn.DataPropertyName = "LastName";
+            this.lastNameDataGridViewTextBoxColumn.HeaderText = "LastName";
+            this.lastNameDataGridViewTextBoxColumn.Name = "lastNameDataGridViewTextBoxColumn";
+            // 
+            // dayDataGridViewTextBoxColumn
+            // 
+            this.dayDataGridViewTextBoxColumn.DataPropertyName = "Day";
+            this.dayDataGridViewTextBoxColumn.HeaderText = "Day";
+            this.dayDataGridViewTextBoxColumn.Name = "dayDataGridViewTextBoxColumn";
+            // 
+            // peopleBindingSource
+            // 
+            this.peopleBindingSource.DataSource = typeof(WindowsFormsApplication2.People);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(576, 333);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Vacation Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.peopleBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        protected internal System.Windows.Forms.Button button2;
+        protected internal System.Windows.Forms.Button button3;
+        private System.Windows.Forms.BindingSource peopleBindingSource;
+        protected internal System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        protected internal System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Year;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dayDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        static WorkerContext db;
+        public Form1()
+        {
+            db = new WorkerContext();
+            //_db = new WorkerContext();
+            InitializeComponent();
+            db.Peoples.Load();
+            dataGridView1.DataSource = db.Peoples.Local.ToBindingList();
+     
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Add addForm = new Add();
+            addForm.numericUpDown2.Value = (int)DateTime.Now.Year;
+            DialogResult result = addForm.ShowDialog(this);
+     
+
+            if (result == DialogResult.Cancel)
+                return;
+          
+            People people = new People();
+            people.Name = addForm.textBox1.Text;
+            people.LastName = addForm.textBox2.Text;
+            people.Day = (int)addForm.numericUpDown1.Value;
+            people.Year = (int)addForm.numericUpDown2.Value;
+            db.Peoples.Add(people);
+            db.SaveChanges();
+            dataGridView1.DataSource = null;
+            dataGridView1.Update();
+            db.Peoples.Load();
+            dataGridView1.DataSource = db.Peoples.AsNoTracking().ToList();
+            dataGridView1.Refresh();
+
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                HolyDay holydayn = new HolyDay();
+                People person=new People();
+                int Daysu;
+                int Dayx;
+                int index = dataGridView1.SelectedRows[0].Index;
+                int id = 0;
+                bool converted = Int32.TryParse(dataGridView1[0, index].Value.ToString(), out id);
+                  if (converted == false)
+                    return;
+                   db.Peoples.AsNoTracking().ToList();
+                     db.Peoples.Load();
+              
+                person=db.Peoples.ToList().FirstOrDefault(i => i.Id == id);
+                db.Entry(person).Reload();
+                db.Entry(person).State = EntityState.Modified;
+                // holydayn.People = db.Peoples.AsNoTracking().ToList().FirstOrDefault(i=>i.Id==id);
+                holydayn.Peopleid = person.Id;
+                AddHol addHolForm = new AddHol(person.Day);
+                DateTime date = new DateTime(person.Year, 1, 1);
+                if (person.Day < 1) {
+                    MessageBox.Show("don`t have weekend!!!");
+                    return;
+                }
+                  addHolForm.dateTimePicker1.Value = date;
+                addHolForm.dateTimePicker2.MaxDate = addHolForm.dateTimePicker1.Value.AddDays(person.Day);
+                addHolForm.dateTimePicker2.MinDate = addHolForm.dateTimePicker1.Value;
+                DialogResult result = addHolForm.ShowDialog(this);
+             
+                holydayn.FirstDate = addHolForm.dateTimePicker1.Value;
+           
+                holydayn.IndexDate = false;
+                addHolForm.dateTimePicker2.MaxDate= addHolForm.dateTimePicker1.Value.AddDays(person.Day);
+              holydayn.SecontDate = addHolForm.dateTimePicker2.Value;
+                 if (result == DialogResult.Cancel)
+                    return;
+                Daysu = person.Day - holydayn.SecontDate.Subtract(holydayn.FirstDate).Days;
+                person.Day = Daysu;
+                holydayn.Days = holydayn.SecontDate.Subtract(holydayn.FirstDate).Days;
+                db.HolyDays.Add(holydayn);
+                db.SaveChanges();
+                dataGridView1.DataSource = null;
+                dataGridView1.Update();
+                db.Peoples.Load();
+                dataGridView1.DataSource = db.Peoples.AsNoTracking().ToList();
+                dataGridView1.Refresh();
+               
+           
+            }
+                
+               
+               
+        }
+      
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0) {
+                int index = dataGridView1.SelectedRows[0].Index;
+                int id = 0;
+                bool converted = Int32.TryParse(dataGridView1[0, index].Value.ToString(), out id);
+                if (converted == false)
+                    return;
+
+                People peopl = db.Peoples.FirstOrDefault(c=> c.Id==id);
+
+                    
+                    
+                    db.Peoples.Remove(peopl);
+                   
+                    db.SaveChanges();
+                dataGridView1.DataSource = null;
+                dataGridView1.Update();
+                db.Peoples.Load();
+                dataGridView1.DataSource = db.Peoples.AsNoTracking().ToList();
+                dataGridView1.Refresh();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+               
+                
+                int index = dataGridView1.SelectedRows[0].Index;
+                int id = 0;
+              
+                bool converted = Int32.TryParse(dataGridView1[0, index].Value.ToString(), out id);
+                date addHolForm = new date(id);
+
+                addHolForm.onupdate += new ONupdate(evenstb);
+                DialogResult result = addHolForm.ShowDialog(this);
+              dataGridView1.Update();
+              dataGridView1.Refresh();
+                if (result == DialogResult.OK) {
+                    addHolForm.Close();
+                   
+                    return;
+                }
+             
+            }
+            dataGridView1.DataSource = null;
+            dataGridView1.Update();
+            db.Peoples.Load();
+            dataGridView1.DataSource = db.Peoples.AsNoTracking().ToList();
+            dataGridView1.Refresh();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+             
+                int id = 0;
+           
+                Add addForm = new Add();
+                int index = 0;
+                if (dataGridView1.RowCount > db.Peoples.Count())
+                {
+                 index = dataGridView1.RowCount - 1;
+                 }
+               else {
+                index = dataGridView1.RowCount;
+                  }
+                for (int i = 0; i < index; i++) {
+                    id = (int)dataGridView1[0, i].Value;
+                People people = new People();
+                People peopleOne = new People();
+                peopleOne = db.Peoples.Find(id);
+                     
+
+                peopleOne.Day = (int)peopleOne.Day+18;
+                peopleOne.Year = (int)peopleOne.Year+1;
+               
+               
+                db.SaveChanges();
+              }
+                dataGridView1.DataSource = null;
+                dataGridView1.Update();
+                db.Peoples.Load();
+                dataGridView1.DataSource = db.Peoples.AsNoTracking().ToList();
+                dataGridView1.Refresh();
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int index = dataGridView1.SelectedRows[0].Index;
+                int id = 0;
+                bool converted = Int32.TryParse(dataGridView1[0, index].Value.ToString(), out id);
+                if (converted == false)
+                    return;
+                People person = new People();
+                person = db.Peoples.Find(id);
+                Sort sort = new Sort(id,person.Name,person.LastName);
+                DialogResult result = sort.ShowDialog(this);
+                if (result == DialogResult.OK)
+                    return;
+            }
+        }
+        private void update(object sender, EventArgs e) {
+
+            this.Refresh();
+        }
+        void evenstb()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.Update();
+            db.Peoples.Load();
+            dataGridView1.DataSource = db.Peoples.AsNoTracking().ToList();
+            dataGridView1.Refresh();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            int id = 0;
+            int index = 0;
+            Add addForm = new Add();
+            if (dataGridView1.RowCount > db.Peoples.Count())
+            {
+                index = dataGridView1.RowCount - 1;
+            }
+            else
+            {
+                index = dataGridView1.RowCount;
+            }
+            for (int i = 0; i < index; i++)
+            {
+                id = (int)dataGridView1[0, i].Value;
+                People people = new People();
+                People peopleOne = new People();
+                peopleOne = db.Peoples.Find(id);
+
+                if (peopleOne.Day < 0)
+                {
+                    peopleOne.Day = 0;
+                    peopleOne.Year = (int)peopleOne.Year - 1;
+                }
+                else { 
+                peopleOne.Day = (int)peopleOne.Day - 18;
+                peopleOne.Year = (int)peopleOne.Year - 1;
+                      }
+                db.SaveChanges();
+            }
+            dataGridView1.DataSource = null;
+            dataGridView1.Update();
+            db.Peoples.Load();
+            dataGridView1.DataSource = db.Peoples.AsNoTracking().ToList();
+            dataGridView1.Refresh();
+        }
+    }
+}
